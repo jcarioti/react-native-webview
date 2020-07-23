0ab4ceea544feafea0cdc2c186ccd41a79dd6eb2
Chris Plantijn <chris@neptunesystems.com>
2020-07-23 16:47:30 -0400

    Enable offscreen pre raster for capable android devices

--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
@@ -181,10 +181,16 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
 
     settings.setAllowFileAccess(false);
     settings.setAllowContentAccess(false);
+
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
       settings.setAllowFileAccessFromFileURLs(false);
       setAllowUniversalAccessFromFileURLs(webView, false);
     }
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      settings.setOffscreenPreRaster(true);
+    }
+
     setMixedContentMode(webView, "never");
 
     // Fixes broken full-screen modals/galleries due to body height being 0.
